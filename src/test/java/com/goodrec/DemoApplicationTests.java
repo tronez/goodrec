f931eb8a30d888ca04ca5610fc19bc596677fd9a
Kamil Polawski <kpolawski@protonmail.com>
2021-01-24 15:22:54 +0100

    Initial commit

---
+++ src/test/java/com/goodrec/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodrec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
